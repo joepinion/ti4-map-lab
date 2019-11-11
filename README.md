53bb228bf454ae862e70157645ff91e423044493
Joe Ellis <joe@plaidhatgames.com>
2019-11-10 23:09:08 -0500

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-To start project for development, from project root:
-yarn install
-yarn start
-
-To build for production:
-yarn build
---
+++ precache-manifest.41af7c8b526a9df368b7c5937fa568cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1446a669bf91bf070b99237cf90b99d0",
+    "url": "/ti4-map-lab/index.html"
+  },
+  {
+    "revision": "8eab433be9f04659b867",
+    "url": "/ti4-map-lab/static/css/main.71cf80b5.chunk.css"
+  },
+  {
+    "revision": "1e9f58993f6d0d1a6985",
+    "url": "/ti4-map-lab/static/js/2.351d865c.chunk.js"
+  },
+  {
+    "revision": "8eab433be9f04659b867",
+    "url": "/ti4-map-lab/static/js/main.f67a2270.chunk.js"
+  },
+  {
+    "revision": "6e44b40d84fabff429b0",
+    "url": "/ti4-map-lab/static/js/runtime~main.03867a53.js"
+  }
+]);
